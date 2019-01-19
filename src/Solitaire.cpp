1b88c7309b645c0436d6445df4102c6fd14f13a7
CaveDries <45296828+cavedries@users.noreply.github.com>
2019-01-19 17:29:48 +0100

    Added foundation printing

--- src/Solitaire.cpp
+++ src/Solitaire.cpp
@@ -8,9 +8,9 @@
 // ------------ Constructor ------------
 // Create game and initialize foundation, tableau and reserve
 Solitaire::Solitaire(std::vector<Card*> tableauCards, std::vector<Card*> reserveCards) :
-		foundation(),
-		tableau(tableauCards),
-		reserve(reserveCards)
+        foundation(),
+        tableau(tableauCards),
+        reserve(reserveCards)
 {
 
 }
@@ -19,29 +19,29 @@ Solitaire::Solitaire(std::vector<Card*> tableauCards, std::vector<Card*> reserve
  * Generate all cards and return them randomly shuffled
  */
 std::vector<Card*> Solitaire::generateCards() {
-	// Get all card values and suits
-	int values[13] = {Card::ACE, Card::TWO, Card::THREE, Card::FOUR, Card::FIVE, Card::SIX, Card::SEVEN, Card::EIGHT, Card::NINE, Card::TEN,
-			Card::JACK, Card::QUEEN, Card::KING};
-	int suits[4] = {Card::HEARTS, Card::SPADES, Card::DIAMONDS, Card::CLUBS};
+    // Get all card values and suits
+    int values[13] = {Card::ACE, Card::TWO, Card::THREE, Card::FOUR, Card::FIVE, Card::SIX, Card::SEVEN, Card::EIGHT, Card::NINE, Card::TEN,
+                      Card::JACK, Card::QUEEN, Card::KING};
+    int suits[4] = {Card::HEARTS, Card::SPADES, Card::DIAMONDS, Card::CLUBS};
 
-	// Generate all cards
-	std::vector<Card*> gameCards;
-	gameCards.reserve(52); // Reserve space in advance
-	for (int suit = 0; suit < 4; suit++) {
-		for (int value = 0; value < 13; value++) {
-			// Generate current card in place and add it to the vector
-			Card* tempCard = new Card(suits[suit], values[value]);
-			gameCards.push_back(tempCard);
-		}
-	}
+    // Generate all cards
+    std::vector<Card*> gameCards;
+    gameCards.reserve(52); // Reserve space in advance
+    for (int suit = 0; suit < 4; suit++) {
+        for (int value = 0; value < 13; value++) {
+            // Generate current card in place and add it to the vector
+            Card* tempCard = new Card(suits[suit], values[value]);
+            gameCards.push_back(tempCard);
+        }
+    }
 
-	// shuffle cards, using the current time as seed for the random generator
-	unsigned seed = std::chrono::system_clock::now()
-						   .time_since_epoch()
-						   .count();
-	shuffle(std::begin(gameCards), std::end(gameCards), std::default_random_engine(seed));
+    // shuffle cards, using the current time as seed for the random generator
+    unsigned int seed = std::chrono::system_clock::now()
+            .time_since_epoch()
+            .count();
+    shuffle(std::begin(gameCards), std::end(gameCards), std::default_random_engine(seed));
 
-	return gameCards;
+    return gameCards;
 }
 
 // ------------ Commands ------------
@@ -49,106 +49,116 @@ std::vector<Card*> Solitaire::generateCards() {
  * Run the game
  */
 void Solitaire::run() {
-	std::cout << "Welcome to Solitaire!" << std::endl;
+    std::cout << "Welcome to Solitaire!" << std::endl;
 
-	std::vector<std::string> moveOptions {
-		"Get new card from Stock",
-		"Move card from Waste to Tableau",
-		"Move card from Waste to Foundation",
-		"Move card from Tableau to Foundation",
-		"Move cards within Tableau"
-	};
+    std::vector<std::string> moveOptions {
+            "Get new card from Stock",
+            "Move card from Waste to Tableau",
+            "Move card from Waste to Foundation",
+            "Move card from Tableau to Foundation",
+            "Move cards within Tableau"
+    };
 
-	bool winnerWinnerChickenDinner = false;
-	while (!winnerWinnerChickenDinner) {
-		printBoard();
+    bool winnerWinnerChickenDinner = false;
+    while (!winnerWinnerChickenDinner) {
+        printBoard();
 
-		// Get user move
-		std::cout << "What would you like to do?" << std::endl;
-		int choice = getUserInput(moveOptions);
+        // Get user move
+        std::cout << "What would you like to do?" << std::endl;
+        int choice = getUserInput(moveOptions);
 
-		// Parse user move
-		switch (choice) {
-			case 0: // card from stock
-				cardFromStock();
-				break;
-			case 1: // waste to tableau
-				wasteToTableau();
-				break;
-			case 2: // waste to foundation
-				wasteToFoundation();
-				break;
-			case 3: // tableau to foundation
-				tableauToFoundation();
-				break;
-			case 4: // move within tableau
-				tableauMove();
-				break;
-			default:
-				std::cout << "A user input error has occured, exiting..." << std::endl;
-				return;
-				break;
-		}
+        // Parse user move
+        switch (choice) {
+            case 0: // card from stock
+                cardFromStock();
+                break;
+            case 1: // waste to tableau
+                wasteToTableau();
+                break;
+            case 2: // waste to foundation
+                wasteToFoundation();
+                break;
+            case 3: // tableau to foundation
+                tableauToFoundation();
+                break;
+            case 4: // move within tableau
+                tableauMove();
+                break;
+            default:
+                std::cout << "A user input error has occured, exiting..." << std::endl;
+                return;
+                break;
+        }
 
-		winnerWinnerChickenDinner = foundation.isGameOver();
-	}
+        winnerWinnerChickenDinner = foundation.isGameOver();
+    }
 
-	std::cout << "Congratulations, you've won!" << std::endl;
+    std::cout << "Congratulations, you've won!" << std::endl;
 
-	return;
+    return;
 }
 
 /**
  * Prints the board to screen
  */
 void Solitaire::printBoard() {
-	Waste wasteStack = this->reserve.getWaste();
-	Stock stockStack = this->reserve.getStock();
+    Waste wasteStack = this->reserve.getWaste();
+    Stock stockStack = this->reserve.getStock();
 
-	// ------------ Stock ------------
-	std::cout << "Number of cards available in the stock: "
-		<< stockStack.getPile().getStack(0)->getCards().size() << std::endl;
+    // ------------ Stock ------------
+    std::cout << "Number of cards available in the stock: "
+              << stockStack.getPile().getStack(0)->getCards().size() << std::endl;
 
-	// ------------ Waste ------------
-	std::vector<Card*> uselessCards = generateCards();
-	wasteStack.addCard(uselessCards[0]); // add a random card, to test functionality
-	std::cout << "\nThe waste:" << std::endl;
-	std::vector<Card *> wasteCards = wasteStack.getStack(0)->getCards();
-	if (wasteCards.empty()) {
-		std::cout << "The waste is empty." << std::endl;
-	} else {
-		for (int index = 0; index < wasteCards.size(); index++) {
-			std::cout << wasteCards[index]->getCard() << std::endl;
-		}
-	}
+    // ------------ Waste ------------
+    std::cout << "\nThe waste:" << std::endl;
+    std::vector<Card *> wasteCards = wasteStack.getStack(0)->getCards();
+    if (wasteCards.empty()) {
+        std::cout << "The waste is empty." << std::endl;
+    } else {
+        for (int index = 0; index < wasteCards.size(); index++) {
+            std::cout << wasteCards[index]->getCard() << std::endl;
+        }
+    }
 
-	// ------------ Foundation ------------
-	std::cout << "\nThe foundation:" << std::endl;
+    // ------------ Foundation ------------
+    std::cout << "\nThe foundation:" << std::endl;
 
-	// save all the cards in a matrix
-	std::vector<std::vector<Card *>> matrix;
-	for (int column = 0; column < 4; column++) {
-		matrix[column] = this->foundation.getStack(column)->getCards();
-	}
+    std::vector<Card *> column1 = foundation.getStack(0)->getCards();
+    std::vector<Card *> column2 = foundation.getStack(1)->getCards();
+    std::vector<Card *> column3 = foundation.getStack(2)->getCards();
+    std::vector<Card *> column4 = foundation.getStack(3)->getCards();
 
-	// print every element of the matrix
-	for (int index = 0; index < 13; index++) {
-		for (int column = 0; column < 4; column++) {
-//				std::cout << matrix[index][column]->getCard();
+        for (int row = 0; row < 13; row++) { // go through every row
+            if (row <= (int) column1.size() - 1) { // row is in range of this column
+                std::cout << column1[row]->getCard() << "\t"; // print the current card
+            } else {
+                std::cout << "--" << "\t"; // print absence of card
+            }
 
-			// preventing nullpointer issues:
-			Card * value = matrix[index][column];
-			int result;
-			(value == nullptr) ? (result = 0) : (result = value->getCard());
-			std::cout << result;
-		}
-		std::cout << std::endl;
-	}
+            if (row <= (int) column2.size() - 1) { // row is in range of this column
+                std::cout << column2[row]->getCard() << "\t";
+            } else {
+                std::cout << "--" << "\t"; // print absence of card
+            }
 
-	// ------------ Tableau ------------
-	std::cout << "\nThe tableau:" << std::endl;
-	std::cout << this->tableau.getStack(0)->getCards().size() << std::endl;
-	std::cout << this->tableau.getStack(1)->getCards().size() << std::endl;
+            if (row <= (int) column3.size() - 1) { // row is in range of this column
+                std::cout << column3[row]->getCard() << "\t";
+            } else {
+                std::cout << "--" << "\t"; // print absence of card
+            }
+
+            if (row <= (int) column4.size() - 1) { // row is in range of this column
+                std::cout << column4[row]->getCard() << "\t";
+            } else {
+                std::cout << "--" << "\t"; // print absence of card
+            }
+            std::cout << std::endl; // move on to next line
+        }
+
+// ------------ Tableau ------------
+    std::cout << "\nThe tableau:" << std::endl;
+    std::cout << this->tableau.getStack(0)->getCards().size() << std::endl;
+    std::cout << this->tableau.getStack(1)->getCards().size() << std::endl;
 
 }
 
@@ -156,133 +166,133 @@ void Solitaire::printBoard() {
  * Prints options to screen, let's user choose, and returns the index of the chosen option.
  */
 int Solitaire::getUserInput(std::vector<std::string> options) {
-	std::cout << "Please select one of the options below:" << std::endl;
-	// Print options
-	for (int i = 0; i < options.size(); i++) {
-		std::cout << i+1 << ") " << options[i] << std::endl;
-	}
+    std::cout << "Please select one of the options below:" << std::endl;
+    // Print options
+    for (int i = 0; i < options.size(); i++) {
+        std::cout << i+1 << ") " << options[i] << std::endl;
+    }
 
-	int choice;
-	bool correctInput = false;
-	while (!correctInput) {
-		std::cin >> choice; // user choice
-		if (1 <= choice && choice <= options.size()) {
-			correctInput = true; // check if input is in selectable range
-		} else {
-			std::cout << "That is not a valid choice. Please try again." << std::endl;
-		}
-	}
+    int choice;
+    bool correctInput = false;
+    while (!correctInput) {
+        std::cin >> choice; // user choice
+        if (1 <= choice && choice <= options.size()) {
+            correctInput = true; // check if input is in selectable range
+        } else {
+            std::cout << "That is not a valid choice. Please try again." << std::endl;
+        }
+    }
 
-	return choice - 1;
+    return choice - 1;
 }
 
 /**
  * Game move methods
  */
 void Solitaire::cardFromStock() {
-	switch (this->reserve.getCard()) {
-		case 0:
-			// Nothing out of the ordinary
-			break;
-		case -1:
-			// Waste and stock are both empty
-			std::cout << "Can't get a new card, waste and stock are empty." << std::endl;
-			break;
-	}
-	return;
+    switch (this->reserve.getCard()) {
+        case 0:
+            // Nothing out of the ordinary
+            break;
+        case -1:
+            // Waste and stock are both empty
+            std::cout << "Can't get a new card, waste and stock are empty." << std::endl;
+            break;
+    }
+    return;
 }
 void Solitaire::wasteToTableau() {
-	std::vector<std::string> tableauColumnOptions {
-		"Column 1",
-		"Column 2",
-		"Column 3",
-		"Column 4",
-		"Column 5",
-		"Column 6",
-		"Column 7"
-	};
+    std::vector<std::string> tableauColumnOptions {
+            "Column 1",
+            "Column 2",
+            "Column 3",
+            "Column 4",
+            "Column 5",
+            "Column 6",
+            "Column 7"
+    };
 
-	// Get user input
-	std::cout << "What column to move the card to?" << std::endl;
-	int tableauColumn = getUserInput(tableauColumnOptions);
+    // Get user input
+    std::cout << "What column to move the card to?" << std::endl;
+    int tableauColumn = getUserInput(tableauColumnOptions);
 
-	// Get card from waste
-	Card* tempCard = reserve.takeCard();
-	if (tempCard == nullptr) {
-		std::cout << "Waste is empty." << std::endl;
-		return;
-	}
+    // Get card from waste
+    Card* tempCard = reserve.takeCard();
+    if (tempCard == nullptr) {
+        std::cout << "Waste is empty." << std::endl;
+        return;
+    }
 
-	// If move failed
-	if (! tableau.addCard(tableauColumn, tempCard)) {
-		std::cout << "Move not possible." << std::endl;
-	}
+    // If move failed
+    if (! tableau.addCard(tableauColumn, tempCard)) {
+        std::cout << "Move not possible." << std::endl;
+    }
 }
 
 void Solitaire::wasteToFoundation() {
-	// Get card from waste
-	Card* tempCard = reserve.takeCard();
-	if (tempCard == nullptr) {
-		std::cout << "Waste is empty." << std::endl;
-		return;
-	}
+    // Get card from waste
+    Card* tempCard = reserve.takeCard();
+    if (tempCard == nullptr) {
+        std::cout << "Waste is empty." << std::endl;
+        return;
+    }
 
-	// If move failed
-	if (! foundation.addCard(tempCard)) {
-		std::cout << "Move not possible." << std::endl;
-	}
+    // If move failed
+    if (! foundation.addCard(tempCard)) {
+        std::cout << "Move not possible." << std::endl;
+    }
 
-	return;
+    return;
 }
 
 void Solitaire::tableauToFoundation() {
-	std::vector<std::string> tableauColumnOptions {
-		"Column 1",
-		"Column 2",
-		"Column 3",
-		"Column 4",
-		"Column 5",
-		"Column 6",
-		"Column 7"
-	};
+    std::vector<std::string> tableauColumnOptions {
+            "Column 1",
+            "Column 2",
+            "Column 3",
+            "Column 4",
+            "Column 5",
+            "Column 6",
+            "Column 7"
+    };
 
-	// Get user input
-	std::cout << "What column top-card to move to the foundations?" << std::endl;
-	int tableauColumn = getUserInput(tableauColumnOptions);
+    // Get user input
+    std::cout << "What column top-card to move to the foundations?" << std::endl;
+    int tableauColumn = getUserInput(tableauColumnOptions);
 
-	// Back up tableau stack
-	std::vector<Card*> backupStack = this->tableau.getStack(tableauColumn)->getCards();
+    // Back up tableau stack
+    std::vector<Card*> backupStack = this->tableau.getStack(tableauColumn)->getCards();
 
-	// Get card
-	Card* tempCard = this->tableau.getStack(tableauColumn)->removeCard();
-	if (tempCard == nullptr) {
-		std::cout << "Waste is empty." << std::endl;
-		return;
-	}
+    // Get card
+    Card* tempCard = this->tableau.getStack(tableauColumn)->removeCard();
+    if (tempCard == nullptr) {
+        std::cout << "Waste is empty." << std::endl;
+        return;
+    }
 
-	// Move tempCard onto foundation
-	// If move failed
-	if (! foundation.addCard(tempCard)) {
-		std::cout << "Move not possible." << std::endl;
-		// Put card back
-		this->tableau.addSet(tableauColumn, backupStack);
-	}
+    // Move tempCard onto foundation
+    // If move failed
+    if (! foundation.addCard(tempCard)) {
+        std::cout << "Move not possible." << std::endl;
+        // Put card back
+        this->tableau.addSet(tableauColumn, backupStack);
+    }
 
 }
 
 void Solitaire::tableauMove() {
-	std::vector<std::string> tableauColumnOptions {
-		"Column 1",
-		"Column 2",
-		"Column 3",
-		"Column 4",
-		"Column 5",
-		"Column 6",
-		"Column 7"
-	};
+    std::vector<std::string> tableauColumnOptions {
+            "Column 1",
+            "Column 2",
+            "Column 3",
+            "Column 4",
+            "Column 5",
+            "Column 6",
+            "Column 7"
+    };
 
-	// Get user input
-	std::cout << "What column to move the card(s) to?" << std::endl;
-	int toColumn = getUserInput(tableauColumnOptions);
+    // Get user input
+    std::cout << "What column to move the card(s) to?" << std::endl;
+    int toColumn = getUserInput(tableauColumnOptions);
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -24,46 +24,46 @@ int old_main() {
 //        std::cout << "takeCard().getSuit(): " << (*reserve.takeCard()).getSuit() << std::endl;
 //    }
 
-	return 0;
+    return 0;
 }
 
 int main() {
-	// Dialog options
-	std::vector<std::string> gameplayOptions {
-		"Yes",
-		"No"
-	};
+    // Dialog options
+    std::vector<std::string> gameplayOptions {
+            "Yes",
+            "No"
+    };
 
-	// Repeat until the user doesn't want to replay again
-	bool replayGame = true;
-	while (replayGame) {
-		// Generate cards
-		std::vector<Card*> gameCards;
-		gameCards = Solitaire::generateCards();
-		//std::cout << "Number of generated cards: " << gameCards.size() << std::endl;
+    // Repeat until the user doesn't want to replay again
+    bool replayGame = true;
+    while (replayGame) {
+        // Generate cards
+        std::vector<Card *> gameCards;
+        gameCards = Solitaire::generateCards();
+//        std::cout << "Number of generated cards: " << gameCards.size() << std::endl;
 
-		// Divide cards
-		std::vector<Card*> tableauCards(gameCards.begin(), gameCards.begin() + 28);
-		std::vector<Card*> reserveCards(gameCards.begin() + 28, gameCards.end());
+        // Divide cards
+        std::vector<Card*> tableauCards(gameCards.begin(), gameCards.begin() + 28);
+        std::vector<Card*> reserveCards(gameCards.begin() + 28, gameCards.end());
 
-		// Initiate and run game
-		Solitaire solitaireGame(tableauCards, reserveCards);
-		solitaireGame.run();
+        // Initiate and run game
+        Solitaire solitaireGame(tableauCards, reserveCards);
+        solitaireGame.run();
 
-		//TODO save highscore
+        //TODO save highscore
 
-		// Ask user for another game
-		// Get user input
-		std::cout << "Would you like to play a new round?" << std::endl;
-		switch (solitaireGame.getUserInput(gameplayOptions)) {
-			case 0:
-				replayGame = true;
-				break;
-			case 1:
-				replayGame = false;
-				break;
-		}
-	}
+        // Ask user for another game
+        // Get user input
+        std::cout << "Would you like to play a new round?" << std::endl;
+        switch (solitaireGame.getUserInput(gameplayOptions)) {
+            case 0:
+                replayGame = true;
+                break;
+            case 1:
+                replayGame = false;
+                break;
+        }
+    }
 
-	return 0;
+    return 0;
 }
